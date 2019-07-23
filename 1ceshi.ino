2b0e86f474c4c40b914a93b05decb29a049de9d8
A-HUAN <48625445+A-HUAN@users.noreply.github.com>
2019-07-23 15:42:51 +0800

    01test
    测试部分
    ▶ 此代码用于测试车轮是否正确连线，以及不同高低电平的小车行进状态。
    ▶ 更改运动控制函数的高低电平(HIGH,LOW)，保证小车烧录代码后能依次执行向前、向后、向左、向右、停止四个运动状态。
    ▶ 若不是正常顺序，更改代码即可，并且记住自己小车的四个状态的高低电平。

---
+++ 1ceshi.ino
@@ -0,0 +1,76 @@
+#include <Servo.h>
+//定义五中运动状态
+#define STOP      0
+#define FORWARD   1
+#define BACKWARD  2
+#define TURNLEFT  3
+#define TURNRIGHT 4
+//定义需要用到的引脚
+int leftMotor1 = 14;
+int leftMotor2 = 15;
+int rightMotor1 = 16;
+int rightMotor2 = 17;
+int leftPWM = 2;
+int rightPWM = 3;
+void setup() {
+  // put your setup code here, to run once:
+  //设置控制电机的引脚为输出状态
+  pinMode(leftMotor1, OUTPUT);
+  pinMode(leftMotor2, OUTPUT);
+  pinMode(rightMotor1, OUTPUT);
+  pinMode(rightMotor2, OUTPUT);
+  pinMode(leftPWM, OUTPUT);
+  pinMode(rightPWM, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int cmd;
+  for(cmd=0;cmd<4;cmd++)//依次执行向前、向后、向左、向右、停止四个运动状态
+  {
+    motorRun(cmd,255);  
+    delay(1000);//每个命令执行2s 
+  } 
+}
+//运动控制函数
+void motorRun(int cmd,int value)
+{
+  analogWrite(leftPWM, value);  //设置PWM输出，即设置速度
+  analogWrite(rightPWM, value);
+  switch(cmd){
+      case 0:
+      Serial.println("FORWARD"); //输出状态
+      digitalWrite(leftMotor1, HIGH);
+      digitalWrite(leftMotor2, LOW);
+      digitalWrite(rightMotor1, LOW);
+      digitalWrite(rightMotor2, HIGH);
+      break;
+      case 1:
+      Serial.println("BACKWARD"); //输出状态
+      digitalWrite(leftMotor1, LOW);
+      digitalWrite(leftMotor2,HIGH );
+      digitalWrite(rightMotor1,HIGH );
+      digitalWrite(rightMotor2,LOW);
+      break;
+      case 2:
+      Serial.println("TURN LEFT"); //输出状态
+      digitalWrite(leftMotor1, HIGH);
+      digitalWrite(leftMotor2, LOW);
+      digitalWrite(rightMotor1, LOW);
+      digitalWrite(rightMotor2, LOW);
+      break;
+    case 3:
+      Serial.println("TURN RIGHT"); //输出状态
+      digitalWrite(leftMotor1, LOW);
+      digitalWrite(leftMotor2, LOW);
+      digitalWrite(rightMotor1, LOW);
+      digitalWrite(rightMotor2, HIGH);
+      break;
+     default:
+      Serial.println("STOP"); //输出状态
+      digitalWrite(leftMotor1, LOW);
+      digitalWrite(leftMotor2, LOW);
+      digitalWrite(rightMotor1, LOW);
+      digitalWrite(rightMotor2, LOW);
+  }
+}
