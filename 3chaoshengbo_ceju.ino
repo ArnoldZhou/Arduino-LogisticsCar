cc003223a64b7fef1ce4bb562a5d4cdb919e9b03
A-HUAN <48625445+A-HUAN@users.noreply.github.com>
2019-07-23 15:44:34 +0800

    03ceju
    超声波测距代码

---
+++ 3chaoshengbo_ceju.ino
@@ -0,0 +1,25 @@
+const int TrigPin = 5; 
+const int EchoPin = 4; 
+float cm; 
+void setup() 
+{ 
+Serial.begin(9600); 
+pinMode(TrigPin, OUTPUT); 
+pinMode(EchoPin, INPUT); 
+} 
+void loop() 
+{ 
+digitalWrite(TrigPin, LOW); //低高低电平发一个短时间脉冲去TrigPin 
+delayMicroseconds(2); 
+digitalWrite(TrigPin, HIGH); 
+delayMicroseconds(10); 
+digitalWrite(TrigPin, LOW); 
+
+
+cm = pulseIn(EchoPin, HIGH) / 58.0; //将回波时间换算成cm 
+cm = (int(cm * 100.0)) / 100.0; //保留两位小数 
+Serial.print(cm); 
+Serial.print("cm"); 
+Serial.println(); 
+delay(1000); 
+}
